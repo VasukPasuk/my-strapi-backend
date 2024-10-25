a5c8f4b4482e62363932d82a4756f14ff9292417
VasukPasuk <vasapaskivskij@gmail.com>
2024-10-25 12:10:35 +0300

    init

---
+++ config/middlewares.ts
@@ -0,0 +1,20 @@
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [process.env.CLIENT_URL], // Your frontend URL
+      methods: '*',
+      headers: '*',
+      credentials: true,
+    }
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
